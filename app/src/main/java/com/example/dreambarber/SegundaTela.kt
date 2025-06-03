d72cf3a1f4f68cecb7450e106ac678bf306145f9
Augusto <augustosantana2005@gmail.com>
2025-06-03 01:04:56 -0300

    Cria interface e navegação

---
+++ app/src/main/java/com/example/dreambarber/SegundaTela.kt
@@ -0,0 +1,24 @@
+package com.example.dreambarber
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dreambarber.databinding.ActivitySegundaTelaBinding
+
+class SegundaTela : AppCompatActivity() {
+    private lateinit var binding: ActivitySegundaTelaBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySegundaTelaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnServicos.setOnClickListener {
+            val navegarterceiraTela = Intent(this, TerceiraTela::class.java)
+            startActivity(navegarterceiraTela)
+        }
+        binding.icPerfil.setOnClickListener {
+            val navegarPerfil = Intent(this, SextaTela::class.java)
+            startActivity(navegarPerfil)
+        }
+    }
+}
